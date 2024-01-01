bcba9d006b48cac4f7a260358476e2af91f39e4f
kevin.krausse <kevin.krausse@taxbit.com>
2023-12-31 18:32:33 -0800

    reformat deps; update build scripts

--- src/kev/dev.clj
+++ src/kev/dev.clj
@@ -1,14 +1,22 @@
 (ns kev.dev
-  (:require
-   [nrepl.cmdline]
-   [shadow.cljs.devtools.api :as shadow.api]
-   [shadow.cljs.devtools.server :as shadow.server]
-   [kev.roam.data-import :as data-import]
-   [shadow.cljs.devtools.cli :as shadow.cli]
-   ))
+  (:require [kev.roam.data-import :as data-import]))
 
 (comment
 
+  (require 'portal.api)
+  (def portal (portal.api/open))
+
+  (do portal)
+
+  (prn "taco")
+
+  (portal.api/inspect
+   [^{:portal.viewer/default :portal.viewer/hiccup}
+                       [:h1 "hello"]
+                       ^{:portal.viewer/default :portal.viewer/hiccup}
+    [:h1 "world"]])
+
+  (data-import/write-datascript-db! "./public/db.edn")
   ;; rebuild db, dev version
   (build-roam-db {:roam-db-path "./public/db.edn"})
 
@@ -16,36 +24,21 @@
   (require '[shadow.cljs.devtools.api :as shadow.api]
            '[shadow.cljs.devtools.server :as shadow.server])
   (shadow.server/start!)
-  (shadow.api/compile :app)
   (shadow.api/watch :app)
-  ;; don't run this, as it will switch the repl type
+  (shadow.api/compile :app)
+
   (shadow.api/nrepl-select :app)
   )
 
-(defn shadow-watch! []
-  (require '[shadow.cljs.devtools.api :as shadow.api]
-           '[shadow.cljs.devtools.server :as shadow.server])
-  (shadow.server/start!)
-  ;; I think this interferes with what is automatically sent?
-;  (shadow.api/compile :app)
-  (shadow.api/watch :app)
- ;;(shadow.api/nrepl-select :app)
-  )
 
-(defn build-roam-db [{:keys [roam-db-path]}]
+(defn build-roam-db [{:keys [db-out-path roam-db-path] :as build-args}]
   (assert roam-db-path "should have roam-db-path specified!")
-  (data-import/write-datascript-db! roam-db-path))
+  (println "building with args" build-args)
+  (time (data-import/write-datascript-db! db-out-path roam-db-path)))
 
 (defn build
   "release-opts will by passsed into shadow. These are essentially the same
   as shadow cli options."
   [{:keys [app roam-db-path release-opts]}]
   ;; call shadow
-  (shadow.api/release! app release-opts))
-
-
-(defn dev-repl [{:keys [cider-args]}]
-  (println "starting server")
-  (shadow-watch!)
-  ;; idk why, but there always seems to be issues on the initial start?
-  (apply nrepl.cmdline/-main cider-args))
+  ((requiring-resolve 'shadow.cljs.devtools.api/release!) app release-opts))
